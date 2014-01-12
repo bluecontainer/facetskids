39e8f1ae008e1a6c307480c988b7d2b84cc421b7
Ben Spark <benspark@gmail.com>
2014-01-12 21:16:13 +0000

    Change production mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,12 @@ FacetsKids::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => ENV["HOST"] }
   # ActionMailer Config
+  config.action_mailer.asset_host = "https://s3.amazonaws.com/facetskids"
+  config.action_mailer.default_url_options = { 
+    :host => ENV["HOST"],
+    :only_path => false 
+  }
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
