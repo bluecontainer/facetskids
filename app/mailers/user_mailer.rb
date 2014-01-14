35beb4833802097b3b796d8a620c43fd2a1ef0fb
Ben Spark <benspark@gmail.com>
2014-01-14 09:26:48 +0000

    Production feedback

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,8 +10,8 @@ class UserMailer < ActionMailer::Base
     @banner_image_type = "app"
     @header_copy = "Adding Facets Kids to your iPad"
     @body_copy = '<b>Facets Kids</b> is the best place to discover fun, exciting, and safe independent films for kids of all ages. Now that you have activated your account, you are almost ready to start streaming films from your iPad.<br/><br/>To start exploring, <b>open this email on your iPad and <a href="' + app_url + '">click here</a></b>. This will allow you to add the Facets Kids app to your home screen.'
-    @button_text = "Go to Facets Kids"
-    @button_link = unauthenticated_root_url
+    @button_text = "Install Facets Kids"
+    @button_link = app_url
     @include_ipad_question = true    
     mail(:to => user.email, :subject => "Getting Started with Facets Kids")
   end
